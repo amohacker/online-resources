4f7f24740b306850b99f204852888665784a9fb1
amohacker <tnvtablet@gmail.com>
2017-04-13 12:55:14 +0200

    update

--- huiswerkscript.js
+++ huiswerkscript.js
@@ -1,6 +1,4 @@
 alert("Uw account is verwijderd.");
-document.getElementById('Body').innerHTML = 'Neem contact op met uw netwerkbeheerder';
-
 function ReplaceContent() {
 document.write('<html><body>Neem contact op met uw netwerkbeheerder</body></html>');
 document.close();
